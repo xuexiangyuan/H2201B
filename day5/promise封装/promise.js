baa87b144172291ea44c842510a3f9ef38be9f42
xuexiangyuan <2441126826@qq.com>
2022-11-18 21:39:25 +0800

    day5

---
+++ day5/promise封装/promise.js
@@ -0,0 +1,42 @@
+// const fs=require('fs')
+
+// const P=new Promise(function(resolve,reject){
+//     fs.readFile('./哈哈.md',(err,data)=>{
+//         if(err) reject(err)
+//         resolve(data)
+//     })
+// })
+// P.then(function(value){
+//     console.log(value.toString());
+// },function(reacin){
+//     console.log('打印失败了');
+// })
+
+// const fs=require('fs')
+
+// const P=new Promise(function(resolve,reject){
+//     fs.readFile('./哈哈.md',(err,data)=>{
+//         if(err) reject(err)
+//         resolve(data)
+//     })
+// })
+// P.then(function(value){
+//     console.log(value.toString());
+// },function(reacin){
+//     console.log('打印失败了');
+// })
+
+
+// const fs=require('fs')
+
+// const P=new Promise(function(resolve,reject){
+//     fs.readFile('./哈哈.md',(err,data)=>{
+//         if(err) reject(err)
+//         resolve(data)
+//     })
+// })
+// P.then(function(value){
+//     console.log(value.toString());
+// },function(reacin){
+//     console.log('打印失败了');
+// })
